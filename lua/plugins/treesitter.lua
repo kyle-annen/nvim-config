6cc02815e48c0b952d52ee5643cfb569ff7d67bc
Kyle Annen <kannen@cpcmarketing.com>
2026-04-07 12:29:16 -0500

    fix: update treesitter config for new API (nvim-treesitter.configs removed in latest version)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,43 +1,49 @@
--- Treesitter: syntax highlighting, autotag
+-- Treesitter: parser installation and syntax highlighting
 return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
-      require('nvim-treesitter.configs').setup({
-        ensure_installed = {
-          'ruby',
-          'typescript',
-          'tsx',
-          'javascript',
-          'html',
-          'css',
-          'scss',
-          'json',
-          'yaml',
-          'lua',
-          'bash',
-          'dockerfile',
-          'markdown',
-          'markdown_inline',
-          'terraform',
-          'elixir',
-          'vim',
-          'vimdoc',
-          'regex',
-          'embedded_template',
-        },
-        sync_install = false,
-        highlight = { enable = true },
-      })
+      -- In modern nvim-treesitter, highlighting is built into Neovim.
+      -- The plugin just manages parser installation.
+      -- Install parsers for the languages we use.
+      local ensure_installed = {
+        'ruby',
+        'typescript',
+        'tsx',
+        'javascript',
+        'html',
+        'css',
+        'scss',
+        'json',
+        'yaml',
+        'lua',
+        'bash',
+        'dockerfile',
+        'markdown',
+        'markdown_inline',
+        'terraform',
+        'elixir',
+        'vim',
+        'vimdoc',
+        'regex',
+        'embedded_template',
+      }
+
+      local installed = require('nvim-treesitter').get_installed()
+      local to_install = vim.tbl_filter(function(lang)
+        return not vim.tbl_contains(installed, lang)
+      end, ensure_installed)
+
+      if #to_install > 0 then
+        require('nvim-treesitter').install(to_install)
+      end
     end,
   },
   {
     'windwp/nvim-ts-autotag',
     event = 'InsertEnter',
-    config = function()
-      require('nvim-ts-autotag').setup()
-    end,
+    opts = {},
   },
 }
